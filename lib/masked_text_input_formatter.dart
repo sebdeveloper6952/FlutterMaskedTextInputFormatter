26fad61d59f8453c67a3ad19fe229d32123d46ad
Jhionan <jhionan@gmail.com>
2019-07-15 17:47:07 -0300

    feature(multiple-separators): change separator param to list<string> to support multiple separators

--- lib/masked_text_input_formatter.dart
+++ lib/masked_text_input_formatter.dart
@@ -5,21 +5,27 @@ import 'package:flutter/services.dart';
 
 class MaskedTextInputFormatter extends TextInputFormatter {
   final String mask;
-  final String separator;
+  final List<String> separators;
 
   MaskedTextInputFormatter({
     @required this.mask,
-    @required this.separator,
-  }) { assert(mask != null); assert (separator != null); }
+    @required this.separators,
+  }) {
+    assert(mask != null);
+    assert(separators != null);
+  }
 
   @override
-  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
-    if(newValue.text.length > 0) {
-      if(newValue.text.length > oldValue.text.length) {
-        if(newValue.text.length > mask.length) return oldValue;
-        if(newValue.text.length < mask.length && mask[newValue.text.length - 1] == separator) {
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text.length > 0) {
+      if (newValue.text.length > oldValue.text.length) {
+        if (newValue.text.length > mask.length) return oldValue;
+        if (newValue.text.length < mask.length &&
+            separators.contains(mask[newValue.text.length - 1])) {
           return TextEditingValue(
-            text: '${oldValue.text}$separator${newValue.text.substring(newValue.text.length-1)}',
+            text:
+            '${oldValue.text}${mask[newValue.text.length-1]}${newValue.text.substring(newValue.text.length - 1)}',
             selection: TextSelection.collapsed(
               offset: newValue.selection.end + 1,
             ),
